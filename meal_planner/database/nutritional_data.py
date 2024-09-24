23823ce66e50047aef175ac083358923b55b4d0b
Devin Beckwith <beckwithdevin@yahoo.com>
2024-09-23 17:40:12 -0700

    Initialized Poetry

---
+++ meal_planner/database/nutritional_data.py
@@ -0,0 +1,75 @@
+import requests
+
+USDA_API_KEY = 'qlv6dd7EHofWCbRtKjBLZYnlyu8ZlSymszQsgWrg'
+USDA_API_URL = 'https://api.nal.usda.gov/fdc/v1/foods/search'
+
+def get_ingredient_data(ingredient_name):
+    params = {
+        'api_key': USDA_API_KEY,
+        'query': ingredient_name,
+        'pageSize': 1,
+        'requireAllWords': True
+    }
+    response = requests.get(USDA_API_URL, params=params)
+    data = response.json()
+
+    if data['totalHits'] > 0:
+        food_data = data['foods'][0]
+        nutrient_data = {
+            'protein_content': None,
+            'fat_content': None,
+            'carbohydrate_content': None
+            # Add other nutritional fields as needed
+        }
+        for nutrient in food_data['foodNutrients']:
+            if nutrient['nutrientName'] == 'Protein':
+                nutrient_data['protein_content'] = nutrient['value']
+            elif nutrient['nutrientName'] == 'Total lipid (fat)':
+                nutrient_data['fat_content'] = nutrient['value']
+            elif nutrient['nutrientName'] == 'Carbohydrate, by difference':
+                nutrient_data['carbohydrate_content'] = nutrient['value']
+            # Add other nutrient mappings as needed
+        return nutrient_data
+    else:
+        return None
+    
+print(get_ingredient_data('broccoli'))
+
+# from database.models import Ingredient
+# from database.database import SessionLocal
+# from usda_api import get_ingredient_data
+
+# def update_ingredient_data():
+#     db = SessionLocal()
+#     ingredients = db.query(Ingredient).all()
+    
+#     for ingredient in ingredients:
+#         nutrient_data = get_ingredient_data(ingredient.name)
+#         if nutrient_data:
+#             ingredient.protein_content = nutrient_data['protein_content']
+#             ingredient.fat_content = nutrient_data['fat_content']
+#             ingredient.carbohydrate_content = nutrient_data['carbohydrate_content']
+#             # Update other nutritional fields as needed
+    
+#     db.commit()
+#     db.close()
+
+# # Update nutritional data for existing ingredients
+# update_ingredient_data()
+
+# # Update nutritional data when a new ingredient is added
+# def create_ingredient(ingredient_data):
+#     db = SessionLocal()
+#     ingredient = Ingredient(**ingredient_data)
+#     db.add(ingredient)
+#     db.commit()
+    
+#     nutrient_data = get_ingredient_data(ingredient.name)
+#     if nutrient_data:
+#         ingredient.protein_content = nutrient_data['protein_content']
+#         ingredient.fat_content = nutrient_data['fat_content']
+#         ingredient.carbohydrate_content = nutrient_data['carbohydrate_content']
+#         # Update other nutritional fields as needed
+#         db.commit()
+    
+#     db.close()
---
+++ meal_planner/unit_tests.py
@@ -0,0 +1,61 @@
+from database import models
+from database.db import SessionLocal
+from database.schemas import Recipe
+from database.crud import get_recipes
+
+import unittest
+from unittest.mock import patch
+
+from services.Hueristic import generate_meal_plan
+
+class TestGenerateMealPlan(unittest.TestCase):
+    recipes = get_recipes(SessionLocal())
+
+    def test_meal_plan_length(self):
+        num_meals = 5
+        meal_plan = generate_meal_plan(num_meals, self.recipes)
+        self.assertEqual(len(meal_plan), num_meals)
+
+    def test_meal_plan_type(self):
+        recipes = self.recipes
+        num_meals = 5
+        meal_plan = generate_meal_plan(num_meals, recipes)
+        self.assertTrue(all(recipe.meal_type == 'main' for recipe in meal_plan))
+
+    def test_meal_plan_uniqueness(self):
+        recipes = self.recipes
+        num_meals = 5
+        meal_plan = generate_meal_plan(num_meals, recipes)
+        self.assertEqual(len(meal_plan), len(set(meal_plan)))
+
+    def test_health_preferences(self):
+        recipes = self.recipes
+        num_meals = 5
+        health_preferences = ['low_fat']
+        meal_plan = generate_meal_plan(num_meals, recipes, health_preferences=health_preferences)
+        self.assertTrue(all(any(health_factor in health_preferences for health_factor in recipe.health_factors) for recipe in meal_plan))
+
+    def test_ingredient_preferences(self):
+        recipes = self.recipes
+        num_meals = 5
+        ingredient_preferences = ['chicken']
+        meal_plan = generate_meal_plan(num_meals, recipes, ingredient_preferences=ingredient_preferences)
+        self.assertTrue(all(any(ingredient in ingredient_preferences for ingredient in recipe.ingredients) for recipe in meal_plan))
+
+    def test_must_cook_recipes(self):
+        recipes = self.recipes
+        num_meals = 5
+        must_cook = ['Adobo Chicken']
+        meal_plan = generate_meal_plan(num_meals, recipes, must_cook=must_cook)
+        self.assertIn(recipes[0], meal_plan)
+
+    def test_randomness(self):
+        recipes = self.recipes
+        num_meals = 5
+        meal_plan1 = generate_meal_plan(num_meals, recipes)
+        meal_plan2 = generate_meal_plan(num_meals, recipes)
+        self.assertNotEqual(meal_plan1, meal_plan2)
+
+if __name__ == '__main__':
+    unittest.main()
+
